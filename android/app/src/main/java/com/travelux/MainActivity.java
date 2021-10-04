68e0533c2cebf4ce1fbbec13f64cd3d9a1ae07d5
YanTee <vanhoinguyen98@gmail.com>
2021-10-04 21:41:20 +0700

    change app name and change app icon

---
+++ android/app/src/main/java/com/travelux/MainActivity.java
@@ -0,0 +1,24 @@
+package com.travelux;
+
+import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash;
+
+import android.os.Bundle;
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Travelux";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // super.onCreate(null);
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); 
+  }
+}
